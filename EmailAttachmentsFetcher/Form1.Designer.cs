ed0f4d76deafe984825b4fe0fc2d16888e3eac7a
codervalx <valc.programmerist@gmail.com>
2023-04-07 02:37:44 +0000

    first commit

---
+++ EmailAttachmentsFetcher/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace EmailAttachmentsFetcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.tboxStatus = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(84, 25);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(164, 61);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Email Attachments Fetcher";
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // tboxStatus
+            // 
+            this.tboxStatus.BackColor = System.Drawing.SystemColors.Menu;
+            this.tboxStatus.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tboxStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tboxStatus.ForeColor = System.Drawing.Color.Green;
+            this.tboxStatus.Location = new System.Drawing.Point(84, 92);
+            this.tboxStatus.Multiline = true;
+            this.tboxStatus.Name = "tboxStatus";
+            this.tboxStatus.Size = new System.Drawing.Size(164, 20);
+            this.tboxStatus.TabIndex = 1;
+            this.tboxStatus.Text = "status";
+            this.tboxStatus.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 138);
+            this.Controls.Add(this.tboxStatus);
+            this.Controls.Add(this.btnStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Email Attachments Fetcher";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.TextBox tboxStatus;
+    }
+}
+
---
+++ EmailAttachmentsFetcher/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using System.Runtime.InteropServices;
+using Timer = System.Timers.Timer;
+using System.Timers;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace EmailAttachmentsFetcher
+{
+    public partial class Form1 : Form
+    {
+        private EmailFetcher emailFetcher;
+        private bool started = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            tboxStatus.Text = "not running";
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (btnStart.Text == "START")
+            {
+                DialogResult result = folderBrowserDialog1.ShowDialog();
+
+                if (result == DialogResult.OK)
+                {
+                    string folderPath = folderBrowserDialog1.SelectedPath;
+                    started = true;
+                    emailFetcher = new EmailFetcher(folderPath);
+                    Hide();
+                    notifyIcon1.Visible = true;
+                    emailFetcher.StartFetch();
+                    btnStart.Text = "END";
+                    tboxStatus.Text = "running";
+                    //Console.WriteLine("Started...");
+                }
+            }
+            else
+            {
+                started = false;
+                emailFetcher.EndFetch();
+                btnStart.Text = "START";
+                tboxStatus.Text = "not running";
+                //Console.WriteLine("Ended...");
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (started)
+            {
+                emailFetcher.EndFetch();
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon1.Visible = true;
+            }
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            Show();
+            notifyIcon1.Visible = false;
+            WindowState = FormWindowState.Normal;
+        }
+    }
+
+    public class EmailFetcher
+    {
+        private Timer fetchTimer;
+        private readonly string folderPath;
+        private readonly string rootFolder = "Fetched Email Attachments";
+        private Outlook.Application outlookApplication;
+        private Outlook.NameSpace outlookNamespace;
+        private Outlook.MAPIFolder inbox;
+        private Outlook.Items items;
+        private List<Outlook.MailItem> unreadMessagesWithAttachments;
+
+        public EmailFetcher(string folderPath)
+        {
+            this.folderPath = folderPath;
+            this.unreadMessagesWithAttachments = new List<Outlook.MailItem>();
+            
+            fetchTimer = new Timer();
+            fetchTimer.Interval = 5000;
+            fetchTimer.Elapsed += FetchTimer_Elapsed;
+        }
+
+        public void StartFetch()
+        {
+            fetchTimer.Start();
+        }
+
+        public void EndFetch()
+        {
+            fetchTimer.Stop();
+            fetchTimer.Dispose();
+
+            Marshal.ReleaseComObject(items);
+            Marshal.ReleaseComObject(inbox);
+            Marshal.ReleaseComObject(outlookNamespace);
+            Marshal.ReleaseComObject(outlookApplication);
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+
+        private async void FetchTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            await FetchEmailWithAttachmentsAsync(folderPath);
+        }
+
+        private async Task FetchEmailWithAttachmentsAsync(string folderPath)
+        {
+            try
+            {
+                string baseDir = Path.Combine(folderPath, rootFolder);
+                if (!Directory.Exists(baseDir))
+                {
+                    Directory.CreateDirectory(baseDir);
+                }
+
+                outlookApplication = new Outlook.Application();
+                outlookNamespace = outlookApplication.GetNamespace("MAPI");
+                inbox = outlookNamespace.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderInbox);
+                items = inbox.Items.Restrict("[UnRead]=true");
+
+                var tasks = new List<Task>();
+                foreach (Outlook.MailItem item in items)
+                {
+                    tasks.Add(Task.Run(() =>
+                    {
+                        string itemSubjectFolder = "";
+                        if (item.Attachments.Count > 0)
+                        {
+                            unreadMessagesWithAttachments.Add(item);
+
+                            foreach (Outlook.Attachment attachment in item.Attachments)
+                            {
+                                string itemDateFolder = Path.Combine(baseDir, item.ReceivedTime.ToString("MM-dd-yyyy"));
+                                itemSubjectFolder = Path.Combine(itemDateFolder, Regex.Replace(item.Subject, @"[^a-zA-Z0-9\s]+", ""));
+
+                                if (!Directory.Exists(itemDateFolder))
+                                {
+                                    Directory.CreateDirectory(itemDateFolder);
+                                }
+                                if (!Directory.Exists(itemSubjectFolder))
+                                {
+                                    Directory.CreateDirectory(itemSubjectFolder);
+                                }
+
+                                attachment.SaveAsFile(Path.Combine(itemSubjectFolder, attachment.FileName));
+                            }
+                            item.SaveAs(Path.Combine(itemSubjectFolder, "mailmsg.msg"));
+                        }
+                    }));
+                }
+                await Task.WhenAll(tasks.ToArray());
+
+                foreach (Outlook.MailItem item in unreadMessagesWithAttachments)
+                {
+                    item.UnRead = false;
+                    item.Save();
+                }
+
+                unreadMessagesWithAttachments.Clear();
+
+                await Task.Delay(1000);
+                //Console.WriteLine("Done...");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occured. {ex}");
+            }
+        }
+    }
+}
